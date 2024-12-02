fd109813e1e5cab56f119ddfcb7bce3a8f0e7822
David Enabs <davidenabs@gmail.com>
2024-12-02 15:54:05 +0100

    "Refactored AddNewPitch component, removed unused code, and updated input fields to use Input component. Also, made minor changes to login.tsx."

--- src/components/AddNewPitch.tsx
+++ src/components/AddNewPitch.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import FileUploadComponent from "./FileUploadComponent";
 import Select from "./forms/select";
 import CreateCategoryModal from "./Modal";
 import { MultiSelect } from "rizzui";
 import pitchClient from "../api/client/pitch";
+import Input from "./forms/input";
 
 const AddNewPitch: React.FC = () => {
   const [fileInput, setFileInput] = useState<File | null>(null);
@@ -49,6 +49,7 @@ const AddNewPitch: React.FC = () => {
     { label: 'Spa', value: 'Spa' },
     { label: 'Restaurant', value: 'Restaurant' },
   ];
+
 
   // Fetch categories from the API
   useEffect(() => {
@@ -125,91 +126,6 @@ const AddNewPitch: React.FC = () => {
 
   const handleFileUpload = (image: File) => setFileInput(image);
 
-  const getCategories = () => {
-    // get the categories from the server
-    // pass it to the select menu for the catgories,
-    // your value shohuld be the id of the category and the label shohuld be thhe name
-
-  }
-
-  // facilities /ammenities should be like thhis
-  // ['gmy','swiing ppol','bar'] 
-
-  // const handleSave = async () => {
-  //   //  all required fields
-  //   const requiredFields = [
-  //     { name: "name", label: "Pitch Name" },
-  //     { name: "amountPerHour", label: "Pitch Price" },
-  //     { name: "discount", label: "Discount" },
-  //     { name: "category_id", label: "Category" },
-  //     { name: "contact", label: "Contact" },
-  //     { name: "openingHours", label: "Opening Hours" },
-  //     { name: "closingHours", label: "Closing Hours" },
-  //     { name: "size", label: "Pitch Size" },
-  //     { name: "pitchManager", label: "Pitch Manager" },
-  //     { name: "ownerId", label: "Owner ID" },
-  //     { name: "location.latitude", label: "Latitude" },
-  //     { name: "location.longitude", label: "Longitude" },
-  //   ];
-
-  //   //  for missing values
-  //   for (const field of requiredFields) {
-  //     const value = field.name.includes(".")
-  //       ? field.name.split(".").reduce((acc, key) => acc[key], formData)
-  //       : formData[field.name];
-  //     if (!value || value.trim() === "") {
-  //       toast.error(`Please provide a value for ${field.label}.`);
-  //       return;
-  //     }
-  //   }
-
-  //   // Validate file input
-  //   if (!fileInput) {
-  //     toast.error("Please upload an image.");
-  //     return;
-  //   }
-
-  //   // Proceed if all validations pass
-  //   const formdata = new FormData();
-  //   formdata.append("name", formData.name);
-  //   formdata.append("amount_per_hour", formData.amountPerHour);
-  //   formdata.append("discount", formData.discount);
-  //   formdata.append("ratings", formData.ratings || "0");
-  //   formdata.append("category_id", formData.category_id);
-  //   formdata.append("contact", formData.contact);
-  //   formdata.append("opening_hours", formData.openingHours);
-  //   formdata.append("closing_hours", formData.closingHours);
-  //   formdata.append("size", formData.size);
-  //   formdata.append("image", fileInput);
-  //   formdata.append("owner_id", formData.ownerId);
-  //   formdata.append("amenities", JSON.stringify(amenities));
-  //   formdata.append("facilities", JSON.stringify(facilities));
-  //   // formdata.append("location", JSON.stringify(formData.location));
-  //   formdata.append("location[latitude]", formData.location.latitude);
-  //   formdata.append("location[longitude]", formData.location.longitude);
-
-  //   const baseUrl = 'http://localhost:8000'//import.meta.env.VITE_BASE_URL;
-  //   const bearerToken = localStorage.getItem("token");
-  //   try {
-
-
-
-  //     const response = await fetch(`${baseUrl}/api/v1/admin/pitches`, {
-  //       method: "POST",
-  //       headers: { Authorization: `Bearer ${bearerToken}` },
-  //       body: formdata,
-  //     });
-  //     console.log(await response.json());
-
-  //     if (!response.ok) throw new Error("Failed to create pitch.");
-  //     const result = await response.json();
-  //     toast.success("Pitch created successfully!");
-  //     console.log("Result:", result);
-  //   } catch (error) {
-  //     toast.error("Error creating pitch.");
-  //     console.error("Error:", error);
-  //   }
-  // };
 
   const handleSave = async () => {
 
@@ -268,9 +184,10 @@ const AddNewPitch: React.FC = () => {
       size: formData.size,
       // pitchManager: parseInt(formData.pitchManager),
       owner_id: 21,
+      gallery: JSON.stringify(gallery)
     };
-    console.log(validatedFormData);
-
+    console.log(gallery);
+    // return;
     try {
       const result = await pitchClient.createPitch(validatedFormData);
       toast.success("Pitch created successfully!");
@@ -330,7 +247,7 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Pitch Name:</td>
                   <td>
-                    <input
+                    <Input
                       type="text"
                       name="name"
                       className="border px-2 py-1"
@@ -342,7 +259,7 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Pitch Size:</td>
                   <td>
-                    <input
+                    <Input
                       type="text"
                       name="size"
                       className="border px-2 py-1"
@@ -354,7 +271,7 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Pitch Price:</td>
                   <td>
-                    <input
+                    <Input
                       type="number"
                       name="amountPerHour"
                       className="border px-2 py-1"
@@ -366,7 +283,7 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Discount:</td>
                   <td>
-                    <input
+                    <Input
                       type="number"
                       name="discount"
                       className="border px-2 py-1"
@@ -378,7 +295,7 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Opening Hours:</td>
                   <td>
-                    <input
+                    <Input
                       type="time"
                       name="openingHours"
                       className="border px-2 py-1"
@@ -390,12 +307,24 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Closing Hours:</td>
                   <td>
-                    <input
+                    <Input
                       type="time"
                       name="closingHours"
                       className="border px-2 py-1"
                       value={formData.closingHours}
                       onChange={handleInputChange}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Images</td>
+                  <td>
+                    <Input
+                      type="file"
+                      name="gallery"
+                      className="border px-2 py-1"
+                      required
+                      onChange={handleFileUpload}
                     />
                   </td>
                 </tr>
@@ -415,7 +344,7 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Pitch Manager:</td>
                   <td>
-                    <input
+                    <Input
                       type="text"
                       name="pitchManager"
                       className="border px-2 py-1"
@@ -427,7 +356,7 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Manager Contact:</td>
                   <td>
-                    <input
+                    <Input
                       type="text"
                       name="contact"
                       className="border px-2 py-1"
@@ -440,7 +369,7 @@ const AddNewPitch: React.FC = () => {
                   <td>Owner:</td>
                   <td>
                     You are to get the list of users with the role pitch other and pass it into a select form. NB the user id should be its value
-                    {/* <input
+                    {/* <Input
                       type="text"
                       name="ownerId"
                       className="border px-2 py-1"
@@ -457,6 +386,7 @@ const AddNewPitch: React.FC = () => {
                       value={categories.find(
                         (opt) => opt.value === formData.category_id
                       )}
+                      className="!px-2 py-1 rounded-full border-[0.4px] border-primary"
                       onChange={(selected) =>
                         handleSelectChange("category_id", selected)
                       }
@@ -474,25 +404,9 @@ const AddNewPitch: React.FC = () => {
                       className="w-full"
                       clearable={true}
                       onClear={() => setAmenities([])}
-                      selectClassName="!h-[56px] rounded-full border-[0.4px] border-primary"
+                      selectClassName="!px-2 py-1 rounded-full border-[0.4px] border-primary"
                       errorClassName="text-red-500"
                     />
-                    {/* <Select
-                      options={[
-                        { value: "wifi", label: "WiFi" },
-                        { value: "parking", label: "Parking" },
-                        { value: "restrooms", label: "Restrooms" },
-                      ]}
-                      isMulti
-                      name="amenities"
-                      onChange={(selected) =>
-                        setFormData((prev) => ({
-                          ...prev,
-                          amenities: selected.map((s) => s.value),
-                        }))
-                      }
-                      className="!py-1"
-                    /> */}
                   </td>
                 </tr>
                 <tr>
@@ -508,38 +422,23 @@ const AddNewPitch: React.FC = () => {
                         className="w-full"
                         clearable={true}
                         onClear={() => setFacilities([])}
-                        selectClassName="!h-[56px] rounded-full border-[0.4px] border-primary"
+                        selectClassName="!px-2 py-1 rounded-full border-[0.4px] border-primary"
                         errorClassName="text-red-500"
                       />
 
                     </div>
-                    {/* <Select
-                      options={[
-                        { value: "gym", label: "Gym" },
-                        { value: "swimming_pool", label: "Swimming Pool" },
-                      ]}
-                      isMulti
-                      name="facilities"
-                      onChange={(selected) =>
-                        setFormData((prev) => ({
-                          ...prev,
-                          facilities: selected.map((s) => s.value),
-                        }))
-                      }
-                      className="!py-1"
-                    /> */}
                   </td>
                 </tr>
                 <tr>
                   <td>Latitude:</td>
                   <td>
-                    <input
+                    <Input
                       type="number"
                       name="latitude"
                       placeholder="Latitude"
                       className="border px-2 py-1"
                       value={formData.location.latitude}
-                      onChange={(e) =>
+                      onChange={(e: any) =>
                         setFormData((prev) => ({
                           ...prev,
                           location: {
@@ -555,13 +454,13 @@ const AddNewPitch: React.FC = () => {
                 <tr>
                   <td>Longitude:</td>
                   <td>
-                    <input
+                    <Input
                       type="number"
                       name="longitude"
                       placeholder="Longitude"
                       className="border px-2 py-1"
                       value={formData.location.longitude}
-                      onChange={(e) =>
+                      onChange={(e: any) =>
                         setFormData((prev) => ({
                           ...prev,
                           location: {
@@ -577,7 +476,7 @@ const AddNewPitch: React.FC = () => {
             </table>
           </div>
 
-          <FileUploadComponent onFileUpload={handleFileUpload} error={null} />
+          {/* <FileUploadComponent onFileUpload={handleFileUpload} error={null} /> */}
         </div>
 
         <div className="flex justify-center mt-5">
